03f3a93edb3c8c7b30acd975695d6920a740d9df
MandarUmare <90469953+MandarUmare@users.noreply.github.com>
2024-01-07 14:40:54 +0530

    gdsg

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const http = require('http');
+const socketIO = require('socket.io');
 
+
+const server = http.createServer(app);
+const io = socketIO(server);
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const Session=require("express-session");
@@ -55,4 +60,9 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+
 module.exports = app;
+
+
+
+
