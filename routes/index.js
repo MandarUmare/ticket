1adbe5b74946eff670e1d417e82fd450d71c8a9d
MandarUmare <90469953+MandarUmare@users.noreply.github.com>
2024-01-07 15:32:54 +0530

    loku

--- routes/index.js
+++ routes/index.js
@@ -10,17 +10,7 @@ const localStrategy=require("passport-local").Strategy;
 passport.use(new localStrategy(userModel.authenticate()));
 
 
-mongoose.set('strictQuery',false);
-const connectDB=async ()=>{
-  try{
-  const conn=await mongoose.connect(process.env.MONGO_URI);
-  console.log(`MongoDB connected : ${conn.connection.host}`);
-  }catch(error)
-  {
-  console.log(error);
-  process.exit(1);
-  }
-}
+
 
 
 /* GET home page. */
